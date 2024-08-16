ca17d4cb8824b90f87674babeaa72175a59c651c
Jim Adamson <jim.adamson@york.ac.uk>
2024-08-16 09:25:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+### 	Alma Analytics / Sentry Integration Daily Members Update via REST API
+
+This is a Powershell script to download patron data from the Ex Libris's Alma Analytics RESTful API, and write the data to a comma-separated (CSV) file,	with the intention that the output file will then be imported by Sentry's Daily Update function, according to the Daily Update file path definition and schedule.
+
+#### Prerequisites
+- A working Alma Analytics report, with expiry dates formatted according to Sentry's expectations, DD/MM/YYYY
+- An application profile must be created on the developers.exlibrisgroup.com site with read permissions for the Analytics API. Create the profile using your Institutional username/password. You will then have an API Key to use with this script.
+
+#### Manual usage
+Although the script is designed to be run as a scheduled task, it might be helpful to run it manually to get a feel for how it works. The first time you run the script, an API key will be requested, so you will want to put this in your paste buffer just before running it.
+
+To begin, open a Powershell window, and `cd` to the directory containing `sentry-daily-update.ps1`. A number of named parameters exist, and should be appended to the base command as follows:
+```
+./sentry-daily-update.ps1 -NamedParam Value
+```
+Mandatory parameters with example values:
+```
+-EmailRecipient john.smith@example.org
+-EmailSender do-not-reply@example.org
+-EmailSmtp smtp.example.org
+-ReportPath "/shared/Example University/Reports/Sentry/Sentry user export"
+```
+
+Optional parameters with example values:
+```
+-ApiKeysDirectoryPath
+-ApiRegion cn
+-BasePath /almaws/v1/analytics/reports
+-BaseUrl https://api-eu.hosted.exlibrisgroup.com
+-EmailSubjectPrefix "Some subject prefix:"
+-OutputFileDirectoryPath .
+-OutputFilename users.csv
+-ProblemRowCount 30000
+-RetryAttempts 10
+-RowLimit 2000
+```
+
+For a full description of the parameters, see the parameter descriptions at the top of the script file.
+
+#### Scheduling this script
+This script can be run automatically by creating a new task in Windows Task Scheduler. The most important pieces of information required for this are the `Command` and `Arguments`. Here's an example:
+
+Command: `C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe`  
+Arguments: `-NoLogo -NoProfile -ExecutionPolicy ByPass -File "C:\path\to\sentry-daily-update.ps1" -EmailSender "do-not-reply@example.org" -EmailRecipient "john.smith@example.org" -EmailSmtp "smtp.example.org" -ReportPath "/shared/Example University/Reports/Sentry/Sentry user export"`  
+Start in: `c:\path\to`
+
+You'll want to schedule this script to run daily at a time JUST BEFORE the Sentry Daily Update is scheduled to run. The script will take a few minutes to run but to be safe allow at least 10 minutes. Note that it's recommended to use double-quotes not single-quotes around parameter values in Windows scheduled tasks, [otherwise odd stuff can happen](https://stackoverflow.com/questions/44594179/pass-powershell-parameters-within-task-scheduler#comment110388918_44594978).
+
+#### Web pages that may be of interest
+- https://developers.exlibrisgroup.com/alma/apis
+- https://developers.exlibrisgroup.com/blog/How-we-re-building-APIs-at-Ex-Libris
+- https://developers.exlibrisgroup.com/alma/apis/analytics
+- https://developers.exlibrisgroup.com/blog/Working-with-Analytics-REST-APIs
+- https://developers.exlibrisgroup.com/blog/alma_sentry_integration
+
+### Local note for UoY Sysadmins
+The specific parameters that we use are in a scheduled task export XML file located in the private repo https://github.com/digital-york/telepen-sentry-scripts-and-conf → `scheduled_tasks_export\Sentry Daily Update.xml` file
---
+++ sentry-daily-update.ps1
@@ -0,0 +1,165 @@
+#Requires -Version 5.1
+<#
+  .SYNOPSIS
+  A Powershell script to download user data from a pre-prepared Alma Analytics report and convert it to CSV format suitable for importing into Sentry-Isis via its "Daily Update" function
+
+  .DESCRIPTION
+  The Sentry-Isis building access management system can import data from a number of different sources as long as the format adheres to the program's requirements. CSV is the format chosen here.
+  This script is designed to be run as a scheduled task shortly before Sentry's Daily Update is scheduled to run, ensuring that Sentry imports a fresh set of data from a CSV file, courtesy of this script.
+
+  .PARAMETER ApiKeysDirectoryPath
+    This script stores and reads the Alma API key in and from a file. This parameter sets the path to that file. By default this is set to a subdirectory called 'auth' in the same directory as this script. [String]
+
+  .PARAMETER ApiRegion
+  Specifies the API region code. Available codes are:
+    Asia Pacific = ap  
+    Canada = ca
+    China = cn
+    Europe = eu (default)
+    North America = na
+
+    Note that when this paramater is specified, the BaseUrl will be automatically modified to include the ApiRegion specified. 
+    By default this is set to eu [String]
+
+  .PARAMETER BaseUrl
+    This sets the Alma API base URL. You will probably not need to explicitly set this at runtime. By default this is set to https://api-eu.hosted.exlibrisgroup.com [String] [Optional]
+
+  .PARAMETER BasePath
+    This sets the Alma API base Path. You will probably not need to explicitly set this at runtime. By default this is set to /almaws/v1/analytics/reports [String] [Optional]
+
+  .PARAMETER EmailRecipient
+    This sets the destination email address for failure related emails to go to. You need to explicitly set this at runtime. There is no default. You can set multiple email addresses by comma-separating them. [String] [Mandatory]
+
+  .PARAMETER EmailSender
+    This sets the sender email address for failure related emails to come from. You need to explicitly set this at runtime. There is no default. [String] [Mandatory]
+
+  .PARAMETER EmailSmtp
+    This sets the SMTP server address to be used for sending failure related emails. You need to explicitly set this at runtime. There is no default. [String] [Mandatory]
+
+  .PARAMETER EmailSubjectPrefix
+    This sets the first part of the subject line for any failure related emails. You will probably not need to explicitly set this at runtime. By default this is set to "Sentry Daily Update:". [String] [Optional]
+
+  .PARAMETER OutputFilename
+    This sets the filename of the output file. You will probably not need to explicitly set this at runtime. By default this is set to daily_update.csv. [String] [Optional]
+
+  .PARAMETER OutputFileDirectoryPath
+    This sets the destination directory path for the output file. The path can be relative to this script file or absolute. You might need to explicitly set this at runtime. By default this is set to the same directory as the scipt file. [String] [Optional]
+
+  .PARAMETER ProblemRowCount
+    This sets the number of minimum number of rows that should be returned before the output is considered "good". This is a sanity check because sometimes the API might consider the download to be complete prematurely. [String] [Optional]
+    By default this is set to 20000. [Int]
+
+  .PARAMETER ReportPath
+    This sets the Alma Analytics report path. The path should be wrapped in quotes. Spaces in the folder names are expected and the required percent encoding is handled automatically. You need to explicitly set this at runtime. There is no default. [String] [Mandatory]
+
+  .PARAMETER RetryAttempts
+    This sets the number of attempts that the script will retry if there is a problem. By default this is set to 5 [Int] [Optional]
+
+  .PARAMETER RowLimit
+    This sets the number of rows to gather per request. There will likely be multiple requests per scrip-run. You will probably not need to explicitly set this at runtime. By default this is set to 1000. [Int] [Optional]
+
+  .EXAMPLE
+  PS> ./sentry-daily-update.ps1 -EmailSender "do-not-reply@example.org" -EmailRecipient "john.smith@example.org" -EmailSmtp "smtp.example.org" -ReportPath "/shared/Example University/Reports/Sentry/Sentry user export"
+#>
+
+param (
+    [string]$ApiKeysDirectoryPath = "$PSScriptRoot\auth",
+    [ValidateSet("ap", "ca", "cn", "eu", "na")]
+    [string]$ApiRegion = "eu",
+    [string]$BasePath = '/almaws/v1/analytics/reports',
+    [string]$BaseUrl = 'https://api-eu.hosted.exlibrisgroup.com',
+    [Parameter(Mandatory=$true)][string]$EmailRecipient,
+    [Parameter(Mandatory=$true)][string]$EmailSender,
+    [Parameter(Mandatory=$true)][string]$EmailSmtp,
+    [string]$EmailSubjectPrefix = "Sentry Daily Update:",
+    [string]$OutputFilename = "daily_update.csv",
+    [string]$OutputFileDirectoryPath = $PSScriptRoot,
+    [int]$ProblemRowCount = 20000,
+    [Parameter(Mandatory=$true)][string]$ReportPath,
+    [int]$RetryAttempts = 5,
+    [int]$RowLimit = 1000
+)
+
+$ErrorActionPreference = "Stop"
+
+If ((Test-Path -Path $ApiKeysDirectoryPath) -ne $true) {
+    Write-Warning "${ApiKeysDirectoryPath} path doesn't exist - creating missing subfolder"
+    $null = New-Item -Type 'directory' -Path "$ApiKeysDirectoryPath" -Force
+}
+
+if (-not (Test-Path "$ApiKeysDirectoryPath\apikey.xml")) {
+    Write-Warning 'The apikey.xml file doesn''t exist'
+    $apikey = Read-Host 'Enter the Ex Libris Alma API key'
+    $apikey | Export-Clixml -Path "$ApiKeysDirectoryPath\apikey.xml" -Force
+}
+
+try {
+    $strApiKey = Import-Clixml -Path "$ApiKeysDirectoryPath\apikey.xml"    
+}
+catch {
+    $($Error[0].Message)
+} finally {
+    if ([string]::IsNullOrEmpty($strApiKey)) {
+        Write-Warning 'API key import issue'
+      }      
+}
+
+if (-not (Test-Path -Path $OutputFileDirectoryPath)) {
+    Write-Host "Output path does not exist"
+    exit 1
+} 
+
+[string]$BaseUrl = $BaseUrl -replace 'api-[^\.]+', "api-$ApiRegion"
+[string]$strUrl = '{0}{1}?path={2}&limit={3}' -f $BaseUrl, $BasePath, [System.Uri]::EscapeDataString($ReportPath), $RowLimit
+[int]$retryCount = 0
+[int]$rowCount = 0
+[bool]$complete = $false
+[bool]$success = $true
+$tmpCsvFile = New-TemporaryFile
+
+do {
+    try {
+        $objRestReq = Invoke-WebRequest -Uri $strUrl -Method Get -Headers @{'Authorization' = "apikey ${strApiKey}"} -TimeoutSec 60
+        if ($objRestReq.StatusCode -eq 200) {
+            $restXml = [xml]$objRestReq.Content
+            $objRows = $restXml.report.QueryResult.ResultXml.rowset.GetElementsByTagName("Row")
+            $objToken = $restXml.report.QueryResult.ResumptionToken
+            $objFin = $restXml.report.QueryResult.IsFinished
+            $objRemoteError = $restXml.web_service_result.errorsExist
+            foreach ($objRow in $objRows) {
+                $rowCount++
+                $csvLine = '"' + ($objRow.Column0, $objRow.Column1, $objRow.Column2, $objRow.Column3 -join '","') + '"'
+                Add-Content -Path $tmpCsvFile -Value $csvLine
+            }
+            if ($objToken) {
+                $strUrl = '{0}{1}?path={2}&limit={3}&token={4}' -f $BaseUrl, $BasePath, [System.Uri]::EscapeDataString($ReportPath), $RowLimit, $objToken
+            }
+            if ($objFin -and $objFin -eq "true") {
+                $complete = $true
+                Copy-Item -Path $tmpCsvFile -Destination "$OutputFileDirectoryPath\$OutputFilename"
+                Remove-Item -Path $tmpCsvFile
+            } elseif ($objRemoteError) {
+                Send-MailMessage -From $EmailSender -To $EmailRecipient -SmtpServer $EmailSmtp -Subject "${EmailSubjectPrefix} XML response error" -Body "Total rows written: ${rowCount}`nError description: ${objRemoteError}"
+                $success = $false
+                break
+            }
+        } else {
+            Send-MailMessage -From $EmailSender -To $EmailRecipient -SmtpServer $EmailSmtp -Subject "${EmailSubjectPrefix} Unexpected HTTP response code" -Body "Total rows written: ${rowCount}`nError description: $(objRestReq.StatusText)"
+            $success = $false
+            break
+        }
+    } catch {
+        $retryCount++
+        if ($retryCount -eq $RetryAttempts) {
+            Send-MailMessage -From $EmailSender -To $EmailRecipient -SmtpServer $EmailSmtp -Subject "${EmailSubjectPrefix} $($_.Exception.GetType().Name)" -Body "Total rows written: ${rowCount}`nError description: $($_.Exception.Message)"
+            $success = $false
+        }
+    }
+    # Here we snooze for 2 seconds between resumptions. As advised at https://developers.exlibrisgroup.com/discussions#!/forum/posts/list/63.page
+    Start-Sleep -Seconds 2
+} until ($complete -or $retryCount -eq $RetryAttempts)
+
+# Handle occasional cases where all the records weren't returned, but the script finished normally
+if ($rowCount -lt $ProblemRowCount -and $success -eq $true) {
+    Send-MailMessage -From $EmailSender -To $EmailRecipient -SmtpServer $EmailSmtp -Subject "${EmailSubjectPrefix} Rows written report" -Body "Total rows written: ${rowCount}"
+}
