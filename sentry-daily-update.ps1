83b59cb03e7cca8982c0b73205c0616cd5e2768a
Jim Adamson <jim.adamson@york.ac.uk>
2026-05-12 17:28:19 +0100

    Replace file-based method of storing API key with TUN.CredentialManager Powershell module
    Numerous other changes, too

--- sentry-daily-update.ps1
+++ sentry-daily-update.ps1
@@ -1,4 +1,3 @@
-#Requires -Version 5.1
 <#
   .SYNOPSIS
   A Powershell script to download user data from a pre-prepared Alma Analytics report and convert it to CSV format suitable for importing into Sentry-Isis via its "Daily Update" function
@@ -7,25 +6,25 @@
   The Sentry-Isis building access management system can import data from a number of different sources as long as the format adheres to the program's requirements. CSV is the format chosen here.
   This script is designed to be run as a scheduled task shortly before Sentry's Daily Update is scheduled to run, ensuring that Sentry imports a fresh set of data from a CSV file, courtesy of this script.
 
-  .PARAMETER ApiKeysDirectoryPath
-    This script stores and reads the Alma API key in and from a file. This parameter sets the path to that file. By default this is set to a subdirectory called 'auth' in the same directory as this script. [String]
-
   .PARAMETER ApiRegion
-  Specifies the API region code. Available codes are:
-    Asia Pacific = ap  
-    Canada = ca
-    China = cn
-    Europe = eu (default)
-    North America = na
+    Specifies the API region code. Available codes are:
+      Asia Pacific = ap
+      Canada = ca
+      China = cn
+      Europe = eu (default)
+      North America = na
 
-    Note that when this paramater is specified, the BaseUrl will be automatically modified to include the ApiRegion specified. 
-    By default this is set to eu [String]
+      Note that when this parameter is specified, the BaseUrl will be automatically modified to include the ApiRegion specified.
+      By default this is set to eu [String]
 
   .PARAMETER BaseUrl
     This sets the Alma API base URL. You will probably not need to explicitly set this at runtime. By default this is set to https://api-eu.hosted.exlibrisgroup.com [String] [Optional]
 
-  .PARAMETER BasePath
-    This sets the Alma API base Path. You will probably not need to explicitly set this at runtime. By default this is set to /almaws/v1/analytics/reports [String] [Optional]
+  .PARAMETER AnalyticsApiBasePath
+    This sets the Alma Analytics API base Path. You will probably not need to explicitly set this at runtime. By default this is set to /almaws/v1/analytics/reports [String] [Optional]
+
+  .PARAMETER AlmaApiKeyIdentifier
+    The identifier for the API key to use when authenticating to the Alma API. [string]
 
   .PARAMETER EmailRecipient
     This sets the destination email address for failure related emails to go to. You need to explicitly set this at runtime. There is no default. You can set multiple email addresses by comma-separating them. [String] [Mandatory]
@@ -41,6 +40,10 @@
 
   .PARAMETER EnableEmail
     This switch parameter enables emailing error messages. When enabled, the parameters EmailRecipient, EmailSender and EmailSmtp become mandatory. [Switch] [Optional]
+
+  .PARAMETER LogFilePath
+    The full file path to a log file to write to. If the file does not already exist it will be automatically created.
+    If the specified file is used across multiple runs, the existing file content will be preserved, with new log entries appended to the file. [string]
 
   .PARAMETER OutputFilename
     This sets the filename of the output file. You will probably not need to explicitly set this at runtime. By default this is set to daily_update.csv. [String] [Optional]
@@ -62,60 +65,102 @@
     This sets the number of rows to gather per request. There will likely be multiple requests per scrip-run. You will probably not need to explicitly set this at runtime. By default this is set to 1000. [Int] [Optional]
 
   .EXAMPLE
-  PS> ./sentry-daily-update.ps1 -EmailSender "do-not-reply@example.org" -EnableEmail -EmailRecipient "john.smith@example.org" -EmailSmtp "smtp.example.org" -ReportPath "/shared/Example University/Reports/Sentry/Sentry user export"
+  PS> ./sentry-daily-update.ps1 -EmailSender "do-not-reply@example.org" -EnableEmail -EmailRecipient "john.smith@example.org" -EmailSmtp "smtp.example.org" -ReportPath "/shared/Example University/Reports/Sentry/Sentry user export" -AlmaServerApiKeyIdentifier "AlmaSentryKey"
 #>
+#Requires -Modules TUN.CredentialManager
+#Requires -Version 5.1
 [CmdletBinding(DefaultParameterSetName = 'logonly')]
 param (
-  [Parameter(ParameterSetName = 'logonly')]
-  [Parameter(ParameterSetName = 'email')]
-  [string]$ApiKeysDirectoryPath = "$PSScriptRoot\auth",
+  [Parameter()]
+  [ValidateNotNullorEmpty()]
+  [string]$AlmaServerApiKeyIdentifier = 'AlmaAPIKeyIdentifier',
 
-  [Parameter(ParameterSetName = 'logonly')]
-  [Parameter(ParameterSetName = 'email')]
   [ValidateSet('ap', 'ca', 'cn', 'eu', 'na')]
+  [ValidateNotNullOrEmpty()]
   [string]$ApiRegion = 'eu',
 
-  [Parameter(ParameterSetName = 'logonly')]
-  [Parameter(ParameterSetName = 'email')]
-  [string]$BasePath = '/almaws/v1/analytics/reports',
+  [ValidateNotNullOrEmpty()]
+  [string]$AnalyticsApiBasePath = '/almaws/v1/analytics/reports',
 
-  [Parameter(ParameterSetName = 'logonly')]
-  [Parameter(ParameterSetName = 'email')]
+  [ValidateNotNullOrEmpty()]
   [string]$BaseUrl = 'https://api-eu.hosted.exlibrisgroup.com',
 
-  [Parameter(ParameterSetName = 'email', Mandatory = $true)]
+  [Parameter(Mandatory, ParameterSetName = 'email')]
+  [ValidateNotNullOrEmpty()]
   [string]$EmailRecipient,
 
-  [Parameter(ParameterSetName = 'email', Mandatory = $true)]
+  [Parameter(Mandatory, ParameterSetName = 'email')]
+  [ValidateNotNullOrEmpty()]
   [string]$EmailSender,
 
-  [Parameter(ParameterSetName = 'email', Mandatory = $true)]
+  [Parameter(Mandatory, ParameterSetName = 'email')]
+  [ValidateNotNullOrEmpty()]
   [string]$EmailSmtp,
 
-  [Parameter(ParameterSetName = 'logonly')]
+  [Parameter(ParameterSetName = 'email')]
+  [ValidateNotNullOrEmpty()]
   [string]$EmailSubjectPrefix = 'Sentry Daily Update:',
 
-  [Parameter(ParameterSetName = 'email', Mandatory = $true)]
+  [Parameter(Mandatory, ParameterSetName = 'email')]
   [switch]$EnableEmail,
 
   [Parameter(ParameterSetName = 'logonly')]
   [Parameter(ParameterSetName = 'email')]
+  [ValidateScript({
+      # Check that the specified path is syntactically correct
+      If (-not $(Test-Path -Path $_ -IsValid)) {
+        Throw 'Syntactically invalid log file path'
+      }
+
+      # Check that the path specified is a file path
+      If (-not $(Split-Path -Path $_ -Leaf | Where-Object { $_ -match '\.' })) {
+        Throw 'Log file path does not appear to be a file path'
+      }
+
+      # Check that the parent folder of the path specified exists
+      If (-not (Test-Path -Path $($_ | Split-Path) -PathType Container)) {
+        Throw 'Specified log file parent folder does not exist'
+      }
+
+      $True
+    })]
   [string]$LogFilePath = '.\sentry-daily-update.log',
 
   [Parameter(ParameterSetName = 'logonly')]
   [Parameter(ParameterSetName = 'email')]
+  [ValidateScript({
+      # Check that the specified path is syntactically correct
+      If (-not $(Test-Path -Path $_ -IsValid)) {
+        Throw 'Syntactically invalid output filename'
+      }
+
+      $True
+    })]
   [string]$OutputFilename = 'daily_update.csv',
 
   [Parameter(ParameterSetName = 'logonly')]
   [Parameter(ParameterSetName = 'email')]
+  [ValidateScript({
+      # Check that the specified path is syntactically correct
+      If (-not $(Test-Path -Path $_ -IsValid)) {
+        Throw 'Syntactically invalid output folder'
+      }
+
+      # Check that the folder exists
+      If (-not (Test-Path -Path $_ -PathType Container)) {
+        Throw 'Specified output folder does not exist'
+      }
+
+      $True
+    })]
   [string]$OutputFileDirectoryPath = $PSScriptRoot,
 
   [Parameter(ParameterSetName = 'logonly')]
   [Parameter(ParameterSetName = 'email')]
   [int]$ProblemRowCount = 20000,
 
-  [Parameter(ParameterSetName = 'logonly', Mandatory = $true)]
-  [Parameter(ParameterSetName = 'email', Mandatory = $true)]
+  [Parameter(Mandatory, ParameterSetName = 'logonly')]
+  [Parameter(Mandatory, ParameterSetName = 'email')]
   [string]$ReportPath,
 
   [Parameter(ParameterSetName = 'logonly')]
@@ -129,49 +174,6 @@ param (
 
 $ErrorActionPreference = 'Stop'
 
-function Test-Paths {
-  param (
-    [string]$LogFilePath,
-    [string]$ApiKeysDirectoryPath,
-    [string]$OutputFileDirectoryPath
-  )
-  if ((Test-Path -Path (Split-Path $LogFilePath)) -ne $true) {
-    Write-Warning "$(Split-Path $LogFilePath) path doesn't exist"
-    exit 1
-  }
-  if ((Test-Path -Path $ApiKeysDirectoryPath) -ne $true) {
-    Write-Warning "${ApiKeysDirectoryPath} path doesn't exist - creating missing subfolder"
-    $null = New-Item -Type 'directory' -Path "$ApiKeysDirectoryPath" -Force
-  }
-  if (-not (Test-Path -Path $OutputFileDirectoryPath)) {
-    Write-Host 'Output path does not exist'
-    exit 1
-  }
-}
-
-function Get-ApiKey {
-  param (
-    [string]$ApiKeysDirectoryPath
-  )
-  if (-not (Test-Path "$ApiKeysDirectoryPath\\apikey.xml")) {
-    Write-Warning 'The apikey.xml file doesn''t exist'
-    $apikey = Read-Host 'Enter the Ex Libris Alma API key'
-    $apikey | Export-Clixml -Path "$ApiKeysDirectoryPath\\apikey.xml" -Force
-  }
-  try {
-    $strApiKey = Import-Clixml -Path "$ApiKeysDirectoryPath\\apikey.xml"
-  }
-  catch {
-    $($Error[0].Message)
-  }
-  finally {
-    if ([string]::IsNullOrEmpty($strApiKey)) {
-      Write-Warning 'API key import issue'
-    }
-  }
-  return $strApiKey
-}
-
 function Send-Email {
   param (
     [string]$EmailSender,
@@ -183,10 +185,33 @@ function Send-Email {
   Send-MailMessage -From $EmailSender -To $($EmailRecipient.Split(',') | ForEach-Object { $_.Trim() }) -SmtpServer $EmailSmtp -Subject $EmailSubject -Body $EmailBody
 }
 
+<#
+    .PARAMETER ApiKeyIdentifier
+    The identifier for the API key to use when authenticating to the Alma API.
+#>
+function Get-WindowsCredentialManagerApiKey {
+  param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullorEmpty()]
+    [string]$ApiKeyIdentifier
+  )
+
+  Try {
+    $credentialObject = Get-StoredCredential -Target $ApiKeyIdentifier -AsCredentialObject -IncludeSecurePassword
+    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($credentialObject.SecurePassword)
+    $plainApikey = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+    Write-Output $plainApikey
+  }
+  Catch {
+    Throw "Failed to retrieve API key for ${ApiKeyIdentifier}"
+  }
+  Finally {
+    Remove-Variable -Name credentialObject, BSTR, plainApikey -ErrorAction SilentlyContinue
+  }
+}
 function Get-Data {
   param (
     [string]$StrUrl,
-    [string]$StrApiKey,
     [string]$OutputFileDirectoryPath,
     [string]$OutputFilename,
     [int]$RowLimit,
@@ -208,9 +233,12 @@ function Get-Data {
     [string]$emailFooter = "`n`nThis email was generated by $(Split-Path -Path $PSCommandPath -Leaf) at $((Get-Date).ToString('dd/MM/yyyy HH:mm:ss')) on $(([System.Net.Dns]::GetHostByName(($env:computerName))).Hostname)." + `
       "`nFor more information on this email, see https://uoy.atlassian.net/wiki/spaces/ittechdocs/pages/43389721/FMSYS+Library+-+Sentry+turnstile+software"
   }
+
+  $plainApikey = Get-WindowsCredentialManagerApiKey -ApiKeyIdentifier $AlmaServerApiKeyIdentifier
+
   do {
     try {
-      $objRestReq = Invoke-WebRequest -Uri $strUrl -Method Get -Headers @{'Authorization' = "apikey ${strApiKey}" } -TimeoutSec 60 -UseBasicParsing
+      $objRestReq = Invoke-WebRequest -Uri $strUrl -Method Get -Headers @{'Authorization' = "apikey ${plainApikey}" } -TimeoutSec 60 -UseBasicParsing
       if ($objRestReq.StatusCode -eq 200) {
         $restXml = [xml]$objRestReq.Content
         $objRows = $restXml.report.QueryResult.ResultXml.rowset.GetElementsByTagName('Row')
@@ -223,7 +251,7 @@ function Get-Data {
           Add-Content -Path $tmpCsvFile -Value $csvLine
         }
         if ($objToken) {
-          $strUrl = '{0}{1}?path={2}&limit={3}&token={4}' -f $BaseUrl, $BasePath, [System.Uri]::EscapeDataString($ReportPath), $RowLimit, $objToken
+          $strUrl = '{0}{1}?path={2}&limit={3}&token={4}' -f $BaseUrl, $AnalyticsApiBasePath, [System.Uri]::EscapeDataString($ReportPath), $RowLimit, $objToken
         }
         if ($objFin -and $objFin -eq 'true') {
           $complete = $true
@@ -261,6 +289,7 @@ function Get-Data {
     # Here we snooze for 2 seconds between resumptions. As advised at https://developers.exlibrisgroup.com/discussions#!/forum/posts/list/63.page
     Start-Sleep -Seconds 2
   } until ($complete -or $retryCount -eq $RetryAttempts)
+  Remove-Variable -Name plainApikey -ErrorAction SilentlyContinue
   if ($rowCount -lt $ProblemRowCount -and $success -eq $true) {
     if ($EnableEmail) {
       Send-Email -EmailSender $EmailSender -EmailRecipient $EmailRecipient -EmailSmtp $EmailSmtp -EmailSubject "${EmailSubjectPrefix} Rows written report" -EmailBody "Total rows written: ${rowCount}${emailFooter}"
@@ -269,13 +298,13 @@ function Get-Data {
   }
 }
 
-Test-Paths -LogFilePath $LogFilePath -ApiKeysDirectoryPath $ApiKeysDirectoryPath -OutputFileDirectoryPath $OutputFileDirectoryPath
-$strApiKey = Get-ApiKey -ApiKeysDirectoryPath $ApiKeysDirectoryPath
+$resolvedLogFilePath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Script:LogFilePath)
+$resolvedOutputFileDirectoryPath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Script:OutputFileDirectoryPath)
+
 $BaseUrl = $BaseUrl -replace 'api-[^\\.]+', "api-$ApiRegion"
-$strUrl = '{0}{1}?path={2}&limit={3}' -f $BaseUrl, $BasePath, [System.Uri]::EscapeDataString($ReportPath), $RowLimit
+$strUrl = '{0}{1}?path={2}&limit={3}' -f $BaseUrl, $AnalyticsApiBasePath, [System.Uri]::EscapeDataString($ReportPath), $RowLimit
 Get-Data -StrUrl $strUrl `
-  -StrApiKey $strApiKey `
-  -OutputFileDirectoryPath $OutputFileDirectoryPath `
+  -OutputFileDirectoryPath $resolvedOutputFileDirectoryPath `
   -OutputFilename $OutputFilename `
   -RowLimit $RowLimit `
   -RetryAttempts $RetryAttempts `
@@ -284,5 +313,5 @@ Get-Data -StrUrl $strUrl `
   -EmailSmtp $EmailSmtp `
   -EmailSubjectPrefix $EmailSubjectPrefix `
   -EnableEmail $EnableEmail `
-  -LogFilePath $LogFilePath `
+  -LogFilePath $resolvedLogFilePath `
   -ProblemRowCount $ProblemRowCount
